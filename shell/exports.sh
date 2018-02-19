78c729ded7f38a4288ae820c222fb562c4976543
nate-wilkins <nate-wilkins@code-null.com>
2018-02-19 12:53:15 -0800

    <f> add env path setup

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
+# -[ env variables ] ----------------------------------------- #
+# /
 export GITHUB_USER=nate-wilkins
+
+
+# -[ path ] -------------------------------------------------- #
+# /
+path_add() {
+    if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
+        export PATH="${PATH:+"$PATH:"}$1"
+    fi
+}
+
+path_add "/usr/local/sbin"
