402f2136acf7ba5ee02c7a0ad94aef91a0eb2668
nate-wilkins <nate-wilkins@code-null.com>
2018-02-27 14:24:36 -0800

    <b:freshrc> make sure we can skip encrypted files

--- install.sh
+++ install.sh
@@ -23,6 +23,8 @@ fi
 DOTFILES_SCHEME=harmonic
 DOTFILES_THEME=dark
 
+DOTFILES_SKIP_ENCRYPTED=no
+
 # -[ parse arguments ]---------------------------------------- #
 # /
 while [[ $# -gt 0 ]]
@@ -35,34 +37,52 @@ do
       shift
       ;;
     -se|--skip-encrypted)
-      SKIP_ENCRYPTED=yes
+      DOTFILES_SKIP_ENCRYPTED=yes
+      shift
+      ;;
+    -s|--scheme)
+      DOTFILES_SCHEME="$2"
+      shift
+      shift
+      ;;
+    -t|--theme)
+      DOTFILES_THEME="$2"
+      shift
       shift
       ;;
     *)
-      echo "Usage:                                                 "
-      echo "  sudo bash setup.sh -u <user-name>                    "
-      echo "                                                       "
-      echo "Options:                                               "
-      echo "  -u|--user              user to provide dotfiles for. "
-      echo "  -se|--skip-encrypted   skips encrypted doftiles.     "
-      echo "                                                       "
-      echo "Example:                                               "
-      echo "  sudo bash setup.sh -u nate-wilkins                   "
-      echo "  sudo bash setup.sh -u john-doe     --skip-encrypted  "
+      echo "Usage:                                                         "
+      echo "  sudo bash install.sh -u <user-name>                          "
+      echo "                                                               "
+      echo "Options:                                                       "
+      echo "  -u | --user                user to provide dotfiles for.     "
+      echo "  -s | --scheme              specify a different color scheme. "
+      echo "  -t | --theme  (dark|light) specify a different color theme.  "
+      echo "  -se| --skip-encrypted      skips encrypted doftiles.         "
+      echo "                                                               "
+      echo "Example:                                                       "
+      echo "  sudo bash install.sh -u nate-wilkins                         "
+      echo "  sudo bash install.sh -u john-doe     --skip-encrypted        "
       shift
       exit 1
       ;;
   esac
 done
 
+# passed in user?
+if [ -z $DOTFILES_USER ]; then
+    echo "Unable to proceed. Script must be given a '--user'"
+    echo "  sudo bash install.sh --user <user>"
+    exit 2
+fi
 
 # -[ dependencies ]------------------------------------------- #
 # /
 # - nodejs
 if hash npm 2>/dev/null; then
     curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
-    chmod +x "/home/$1/.nvm/nvm.sh"
-    . "/home/$1/.nvm/nvm.sh"
+    chmod +x /home/$DOTFILES_USER/.nvm/nvm.sh
+    . /home/$DOTFILES_USER/.nvm/nvm.sh
     nvm install node
 fi
 
@@ -75,8 +95,8 @@ npm install --global base16-builder
 mkdir -p .theme/
 
 # - spacemacs
-mkdir -p ~/.spacemacs.d/private/local/base16-$($DOTFILES_SCHEME)16-$($DOTFILES_THEME)-theme
-SPACEMACS_THEME=~/.spacemacs.d/private/local/base16-$($DOTFILES_SCHEME)16-$($DOTFILES_THEME)-theme/base16-$($DOTFILES_SCHEME)16-$($DOTFILES_THEME).el
+mkdir -p ~/.spacemacs.d/private/local/base16-${DOTFILES_SCHEME}16-${DOTFILES_THEME}-theme
+SPACEMACS_THEME=~/.spacemacs.d/private/local/base16-${DOTFILES_SCHEME}16-${DOTFILES_THEME}-theme/base16-${DOTFILES_SCHEME}16-${DOTFILES_THEME}.el
 [ -f $SPACEMACS_THEME ] && rm -f $SPACEMACS_THEME
 base16-builder -s $DOTFILES_SCHEME -b $DOTFILES_THEME -t emacs > $SPACEMACS_THEME
 
@@ -95,14 +115,19 @@ base16-builder -s $DOTFILES_SCHEME -b $DOTFILES_THEME -t termite > .theme/termit
 
 # -[ fresh ]-------------------------------------------------- #
 # /
-[ -d /home/$1/.fresh ]   && rm -rf /home/$1/.fresh
-[ -f /home/$1/.freshrc ] && rm -f  /home/$1/.freshrc
-find /home/$1/ -lname "/home/$1/.fresh/*" -delete
+[ -d /home/$DOTFILES_USER/.fresh ]   && rm -rf /home/$DOTFILES_USER/.fresh
+[ -f /home/$DOTFILES_USER/.freshrc ] && rm -f  /home/$DOTFILES_USER/.freshrc
+find /home/$DOTFILES_USER/ -lname /home/$DOTFILES_USER/.fresh/* -delete
 
-cp /home/$1/.dotfiles/.freshrc /home/$1/.freshrc
+cp /home/$DOTFILES_USER/.dotfiles/.freshrc /home/$DOTFILES_USER/.freshrc
 
-FRESH_LOCAL_SOURCE=$1/dotfiles
-runuser -l $1 -c 'source <(curl -sL https://get.freshshell.com)'
+# write variables for fresh
+[ -f /home/$DOTFILES_USER/.freshvars ] && rm -f /home/$DOTFILES_USER/.freshvars
+echo "DOTFILES_SKIP_ENCRYPTED=${DOTFILES_SKIP_ENCRYPTED}" > /home/$DOTFILES_USER/.freshvars
+chmod +x /home/$DOTFILES_USER/.freshvars
+
+  FRESH_LOCAL_SOURCE=$DOTFILES_USER/dotfiles
+  runuser -l $DOTFILES_USER -c 'source <(curl -sL https://get.freshshell.com)'
 
 # sourced in .zshrc
-chmod +x /home/$1/.fresh/build/shell.sh
+chmod +x /home/$DOTFILES_USER/.fresh/build/shell.sh
