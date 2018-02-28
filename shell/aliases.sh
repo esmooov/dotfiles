ddf21eae1e00dc9a4860dce11e1abfa63a77d076
nate-wilkins <nate-wilkins@code-null.com>
2018-02-27 16:47:12 -0800

    <f:aliases.sh> exa aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# system
+alias ls="exa --group-directories-first"
+alias l="exa --long --group-directories-first --group --git"
+alias la="exa --long --group-directories-first --all --group --git"
+alias tree="exa -T"
+
 # emacs
 alias view="emacs --insert /dev/stdin"
 
