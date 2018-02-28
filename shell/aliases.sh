edd2ead1ec94d23a0f3ad4942ba059350ee05ef8
nate-wilkins <nate-wilkins@code-null.com>
2018-02-27 23:08:19 -0800

    <f:aliases.sh> git and lwp-request aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 
-# system
+# - system
 alias ls="exa --group-directories-first"
 alias l="exa --long --group-directories-first --group --git"
 alias la="exa --long --group-directories-first --all --group --git"
 alias tree="exa -T"
 
-# emacs
+# - emacs
 alias view="emacs --insert /dev/stdin"
 
-# network
+# - web
 alias internet="ping 8.8.8.8"
-
-# web
 alias chrome="google-chrome"
+
+# HTTP requests by @janmoesen
+for method in GET HEAD POST PUT DELETE TRACE OPTIONS; do
+	  alias "r-$method"="lwp-request -m '$method'"
+done
+
+# - git
+# `cd` to git repository root
+# Taken from: sapegin/dotfiles
+alias gr='git rev-parse 2>/dev/null && cd "./$(git rev-parse --show-cdup)"'
